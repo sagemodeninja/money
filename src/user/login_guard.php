721933ec67dd05e13c7f2a2e2225f24b14eae6ab
sagemodeninja <gary.antier@rafi.ph>
2022-10-09 15:52:28 +0800

    fix: dynamic redirect uri

--- src/user/login_guard.php
+++ src/user/login_guard.php
@@ -4,11 +4,11 @@
         include_once $_SERVER['DOCUMENT_ROOT'] . "/secrets.php";
 
         $isSignedIn = @$_SESSION["user_is_signed-in"];
-        $client_id =  GOOGLE_CLIENT_ID;
-        $redirect_uri = GetHost() . "/user/verify_google.php";
+        $clientId =  GOOGLE_CLIENT_ID;
+        $redirectURI = getFullHost() . "/user/verify_google.php";
         
         if(!isset($isSignedIn)) {
-            header("location: https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=$client_id&redirect_uri=$redirect_uri&scope=openid+email+profile");
+            header("location: https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=$clientId&redirect_uri=$redirectURI&scope=openid+email+profile");
         }
     }
 ?>
