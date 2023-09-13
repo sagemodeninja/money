3d7257799d0042fc1fd6418591f387cc8eb21054
sagemodeninja <gary.antier@rafi.ph>
2023-09-13 11:02:25 +0800

    feat: use webpack

---
+++ src/assets/scripts/app.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./scripts/app.ts ***!
+  \************************/
+console.log('Hello World!');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsT0FBTyxDQUFDLEdBQUcsQ0FBQyxjQUFjLENBQUMsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL2UtbW9uZXkvLi9zY3JpcHRzL2FwcC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnSGVsbG8gV29ybGQhJyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
