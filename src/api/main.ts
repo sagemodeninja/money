dcdff29f7686ccaafc5362202ab60ec4518e96d8
Gary Antier <contact@garyantier.com>
2025-07-20 15:17:12 +0800

    feat: basic route handler

--- src/api/main.ts
+++ src/api/main.ts
@@ -1,11 +1,20 @@
 import { parseArgs } from './utils/args.ts'
+import { Router } from './router.ts'
 
 const args = parseArgs(Deno.args)
 
 if (!args.success) {
     const  { code, message } = args.error
-    console.error(`Error: ${code}: ${message}`)
+    console.error(`${code}: ${message}`)
     Deno.exit(1)
 }
 
-Deno.serve(() => new Response('Hello World!'))
+const router = new Router()
+
+router.get('/login', async () => {
+    return new Response(JSON.stringify({ data: 'Hello World!' }), {
+        headers: { 'Content-Type': 'application/json' }
+    });
+});
+
+Deno.serve(router.handler)
